6cfd1c89171aa1b3d1aec1cb1c1a625dd897d501
Mrunal Alatkar <alatkar.mrunal@gmail.com>
2023-12-28 10:32:01 +0000

    Fixed vue websocket issue

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: { 
-    allowedHosts: ['.gitpod.io']
-  }
+    allowedHosts: ['.gitpod.io'],
+    client: {
+      webSocketURL: {
+        port: process.env.GITPOD_WORKSPACE_ID ? 443 : undefined,
+      },
+    },
+  },
 })
